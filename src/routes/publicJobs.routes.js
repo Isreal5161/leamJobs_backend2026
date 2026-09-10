a431a853d60d914722c4d188a850f3cc2cadc0d7
Isreal5161 <diatecktechnology@gmail.com>
2026-09-10 20:03:34 +0100

    expose approved public jobs list

--- src/routes/publicJobs.routes.js
+++ src/routes/publicJobs.routes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
-import { getPublicJob } from '../controllers/seekerJobs.controller.js';
+import { getPublicJob, listJobs } from '../controllers/seekerJobs.controller.js';
+import { validateSeekerJobsQuery } from '../validators/seekerJobs.validation.js';
 
 const publicJobsRouter = Router();
 
+publicJobsRouter.get('/', validateSeekerJobsQuery, listJobs);
 publicJobsRouter.get('/:jobId', getPublicJob);
 
 export default publicJobsRouter;
